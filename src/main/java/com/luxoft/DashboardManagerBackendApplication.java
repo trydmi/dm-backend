231114d449555b8cf99fbf332b2ced6286ed0828
Dmytro Pushkar <trydmi.hf@gmail.com>
2022-12-19 16:57:33 +0200

    first commit

---
+++ src/main/java/com/luxoft/DashboardManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardManagerBackendApplication.class, args);
+    }
+
+}
